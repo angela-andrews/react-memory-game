13f7d0d5ddde7910a8a53c0f3d37905567bc8d33
Angela Andrews <aandrews17@yahoo.com>
2018-09-28 12:57:56 -0400

    broke sections in to components and added logic

--- src/components/Wrapper/Wrapper.js
+++ src/components/Wrapper/Wrapper.js
@@ -1,6 +1,10 @@
 import React from "react";
 import "./Wrapper.css";
 
-const Wrapper = props => <div className="wrapper">{props.children}</div>;
-
+const Wrapper = props => 
+<React.Fragment>
+    
+    <div className="wrapper">
+    {props.children}</div>;
+</React.Fragment>
 export default Wrapper;
